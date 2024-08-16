260bbb19877bb97f6a495c792fe6bf38ec96bafa
marmulle <marmulle@student.hive.fi>
2024-08-16 12:32:49 +0300

    add exercise 3.8

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ var morgan = require("morgan");
 
 const app = express();
 
+morgan.token("data", function (req, res) {
+	return req.method == "POST" ? JSON.stringify(req.body) : " ";
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+	morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
 	{
